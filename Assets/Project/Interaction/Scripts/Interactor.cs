45abb23fda3e619fbd5149fcdf40d0931b599883
Javier Matejko <javiermatejko@gmail.com>
2026-05-01 16:55:55 -0300

    Added interaction system

---
+++ Assets/Project/Interaction/Scripts/Interactor.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    private readonly List<IInteractable> _inRange = new();
+
+    public void TryInteract()
+    {
+        var target = GetBestTarget();
+
+        if (target != null)
+        {
+            target.Interact(this);
+        }
+    }
+
+    private IInteractable GetBestTarget()
+    {
+        float bestDistance = float.MaxValue;
+        IInteractable best = null;
+
+        foreach (var interactable in _inRange)
+        {
+            if (interactable == null) continue;
+
+            MonoBehaviour mb = interactable as MonoBehaviour;
+            float dist = Vector3.Distance(transform.position, mb.transform.position);
+
+            if (dist < bestDistance)
+            {
+                bestDistance = dist;
+                best = interactable;
+            }
+        }
+
+        return best;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var interactable = other.GetComponent<IInteractable>();
+
+        if (interactable != null && !_inRange.Contains(interactable))
+        {
+            _inRange.Add(interactable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var interactable = other.GetComponent<IInteractable>();
+
+        if (interactable != null)
+        {
+            _inRange.Remove(interactable);
+        }
+    }
+}
